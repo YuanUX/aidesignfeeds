c7823dc5a33e5d2beeea1387b1f74efcb92530f9
YuanUX <113670202+YuanUX@users.noreply.github.com>
2025-10-24 21:07:44 -0700

    update the layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://aidesignfeeds.com',
-  output: 'static',
+  output: 'server',
+    site: 'https://aidesignfeeds.com', // must be full URL with protocol
+  adapter: cloudflare({
+    platform: 'workers',
+    sessions: { storage: 'cookie' }, // <-- no KV needed
+  }),
   integrations: [mdx(), sitemap(), tailwind()],
   vite: {
     ssr: { external: ['rss-parser'] },
     optimizeDeps: { exclude: ['rss-parser'] },
   },
 });
+
